1f2306a1e74d9145de2d3ddca0d74ea85388afa2
sergiopan19 <sp19p@my.fsu.edu>
2025-11-10 15:22:35 -0600

    update base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/pomodoro-timer',
+  base: "/pomodoro-timer/",
 })
